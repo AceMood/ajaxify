05bb3c9f5d73c1cd709ba6e7283f96b54727c31d
AceMood <zmike86@163.com>
2015-09-08 02:14:55 +0800

    dev

--- src/utils.js
+++ src/utils.js
@@ -9,8 +9,8 @@ var slice = Array.prototype.slice;
 
 /**
  * 对象混入
- * @param {!Object} target
- * @param {*} var_args
+ * @param {!Object} target 目标对象
+ * @param {*} var_args 列表对象集合
  */
 function extend (target, var_args) {
   var objs = slice.call(arguments, 1);
@@ -27,9 +27,9 @@ function extend (target, var_args) {
 }
 
 /**
- * 标识是否Html5History是被支持的. 其实就是看window.history.pushState
- * @param {Window=} opt_win Optional window to check.
- * @return {boolean} 是否支持html5 history.
+ * 标识是否Html5History是被支持的。
+ * @param {Window=} opt_win 要检查的window对象
+ * @return {boolean} 是否支持html5 history
  */
 function isSupportedHistory (opt_win) {
   var win = opt_win || window;
